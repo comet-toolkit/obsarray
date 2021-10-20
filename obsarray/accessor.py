a0dc4f63548939767734e7d23df81e2e18b20ae7
shunt16 <sam.hunt@npl.co.uk>
2021-10-20 16:33:56 +0100

    add minimal functionality for add/retrieving uncertainty variables, sum of squares

--- obsarray/accessor.py
+++ obsarray/accessor.py
@@ -11,21 +11,50 @@ class UncAccessor(object):
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
-    @property
-    def measured_variables(self):
-        measured_variables = []
-        for v in self._obj.variables:
-            if "u_components" in self._obj[v].attrs:
-                measured_variables.append(v)
-        return measured_variables
+    def var_unc_var_names(self, obs_var_name):
+        return self._obj[obs_var_name].attrs["u_components"] if "u_components" in self._obj[obs_var_name].attrs else []
+
+    def is_obs_var(self, var_name):
+        if self.var_unc_var_names(var_name):
+            return True
+        return False
 
     @property
-    def uncertainty_variables(self):
-        u_components = set()
-        for v in self._obj.variables:
-            if "u_components" in self._obj[v].attrs:
-                u_components |= set(self._obj[v].attrs["u_components"])
-        return list(u_components)
+    def obs_vars(self):
+        obs_var_names = []
+        for var_name in self._obj.variables:
+            if self.is_obs_var(var_name):
+                obs_var_names.append(var_name)
+        return self._obj[obs_var_names].data_vars
+
+    @property
+    def unc_vars(self):
+        unc_var_names = set()
+        for var_name in self.obs_vars:
+            unc_var_names |= set(self.var_unc_var_names(var_name))
+        return self._obj[list(unc_var_names)].data_vars
+
+    def is_unc_var(self, var_name):
+        if var_name in self.unc_vars:
+            return True
+        return False
+
+    def var_unc_vars(self, obs_var_name):
+        return self._obj[self.var_unc_var_names(obs_var_name)].data_vars
+
+    def quadsum(self):
+        return sum(d for d in (self._obj ** 2).data_vars.values()) ** 0.5
+
+    def u_tot(self, obs_var):
+        return self.var_unc_vars(obs_var)._dataset.unc.quadsum()
+
+    def add_unc_var(self, obs_var, unc_def):
+        self._obj[unc_def[0]] = unc_def[1]
+
+        if self.is_obs_var(obs_var):
+            self._obj[obs_var].attrs["u_components"].append(unc_def[0])
+        else:
+            self._obj[obs_var].attrs["u_components"] = [unc_def[0]]
 
 
 if __name__ == "__main__":
--- obsarray/test/test_accessor.py
+++ obsarray/test/test_accessor.py
@@ -9,44 +9,102 @@ import obsarray
 __author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
 __all__ = []
 
-np.random.seed(0)
-temperature = 15 + 8 * np.random.randn(2, 2, 3)
-u_r_temperature = temperature * 0.02
-u_s_temperature = temperature * 0.01
-precipiation = 15 + 8 * np.random.randn(2, 2, 3)
-u_r_precipiation = temperature * 0.02
-lon = [[-99.83, -99.32], [-99.79, -99.23]]
-lat = [[42.25, 42.21], [42.63, 42.59]]
-time = pd.date_range("2014-09-06", periods=3)
-reference_time = pd.Timestamp("2014-09-05")
-
-ds = xr.Dataset(
-    data_vars=dict(
-        temperature=(["x", "y", "time"], temperature),
-        u_r_temperature=(["x", "y", "time"], u_r_temperature),
-        u_s_temperature=(["x", "y", "time"], u_s_temperature),
-        precipitation=(["x", "y", "time"], precipiation),
-        u_r_precipitation=(["x", "y", "time"], u_r_precipiation),
-    ),
-    coords=dict(
-        lon=(["x", "y"], lon),
-        lat=(["x", "y"], lat),
-        time=time,
-        reference_time=reference_time,
-    ),
-    attrs=dict(description="Weather related data."),
-)
-
-ds.temperature.attrs["u_components"] = ["u_r_temperature", "u_s_temperature"]
-ds.precipitation.attrs["u_components"] = ["u_r_precipitation"]
-
 
 class TestUncAccessor(unittest.TestCase):
-    def test_measurement_variables(self):
-        self.assertCountEqual(ds.unc.measured_variables, ["temperature", "precipitation"])
+    def setUp(self):
+        np.random.seed(0)
+        temperature = 15 + 8 * np.random.randn(2, 2, 3)
+        u_r_temperature = temperature * 0.02
+        u_s_temperature = temperature * 0.01
+        precipiation = 15 + 8 * np.random.randn(2, 2, 3)
+        u_r_precipiation = temperature * 0.02
+        lon = [[-99.83, -99.32], [-99.79, -99.23]]
+        lat = [[42.25, 42.21], [42.63, 42.59]]
+        time = pd.date_range("2014-09-06", periods=3)
+        reference_time = pd.Timestamp("2014-09-05")
 
-    def test_uncertainty_variables(self):
-        self.assertCountEqual(ds.unc.uncertainty_variables, ["u_r_temperature", "u_s_temperature", "u_r_precipitation"])
+        self.ds = xr.Dataset(
+            data_vars=dict(
+                temperature=(["x", "y", "time"], temperature),
+                u_r_temperature=(["x", "y", "time"], u_r_temperature),
+                u_s_temperature=(["x", "y", "time"], u_s_temperature),
+                precipitation=(["x", "y", "time"], precipiation),
+                u_r_precipitation=(["x", "y", "time"], u_r_precipiation),
+            ),
+            coords=dict(
+                lon=(["x", "y"], lon),
+                lat=(["x", "y"], lat),
+                time=time,
+                reference_time=reference_time,
+            ),
+            attrs=dict(description="Weather related data."),
+        )
+
+        self.ds.temperature.attrs["u_components"] = ["u_r_temperature", "u_s_temperature"]
+        self.ds.precipitation.attrs["u_components"] = ["u_r_precipitation"]
+
+    def test_var_unc_var_names(self):
+        self.assertCountEqual(self.ds.unc.var_unc_var_names("temperature"), ["u_r_temperature", "u_s_temperature"])
+
+    def test_var_unc_var_names_none(self):
+        self.assertCountEqual(self.ds.unc.var_unc_var_names("u_r_temperature"), [])
+
+    def test_is_obs_var_true(self):
+        self.assertTrue(self.ds.unc.is_obs_var("temperature"))
+
+    def test_is_obs_var_false(self):
+        self.assertFalse(self.ds.unc.is_obs_var("u_r_temperature"))
+
+    def test_obs_vars(self):
+        obs_vars = self.ds.unc.obs_vars
+        self.assertEqual(type(obs_vars), xr.core.dataset.DataVariables)
+        self.assertCountEqual(list(obs_vars.variables), ["temperature", "precipitation"])
+
+    def test_unc_vars(self):
+        unc_vars = self.ds.unc.unc_vars
+        self.assertEqual(type(unc_vars), xr.core.dataset.DataVariables)
+        self.assertCountEqual(list(unc_vars.variables), ["u_r_temperature", "u_s_temperature", "u_r_precipitation"])
+
+    def test_is_unc_var_true(self):
+        self.assertTrue(self.ds.unc.is_unc_var("u_r_temperature"))
+
+    def test_is_unc_var_false(self):
+        self.assertFalse(self.ds.unc.is_unc_var("temperature"))
+
+    def test_var_unc_vars(self):
+        unc_vars = self.ds.unc.var_unc_vars("temperature")
+        self.assertEqual(type(unc_vars), xr.core.dataset.DataVariables)
+        self.assertCountEqual(list(unc_vars.variables), ["u_r_temperature", "u_s_temperature"])
+
+    def test_var_unc_vars_none(self):
+        unc_vars = self.ds.unc.var_unc_vars("u_r_temperature")
+        self.assertEqual(type(unc_vars), xr.core.dataset.DataVariables)
+        self.assertCountEqual(list(unc_vars.variables), [])
+
+    def test_quadsum(self):
+
+        test_ds = xr.Dataset(
+            data_vars=dict(
+                a=(["x"], np.ones(3)*3),
+                b=(["x"], np.ones(3)*4),
+            )
+        )
+        c = test_ds.unc.quadsum()
+
+        np.testing.assert_array_equal(c.values, np.ones(3)*5)
+
+    def test_u_tot(self):
+        u_tot_temperature = self.ds.unc.u_tot("temperature")
+        u_tot_temperature_test = (self.ds.u_r_temperature ** 2. + self.ds.u_s_temperature ** 2.0) ** 0.5
+
+        np.testing.assert_array_equal(u_tot_temperature.values, u_tot_temperature_test.values)
+
+    def test_add_unc_var(self):
+        u_s_precipitation = xr.DataArray(self.ds.precipitation.values, dims=["x", "y", "time"])
+        self.ds.unc.add_unc_var("precipitation", ("u_s_precipitation", u_s_precipitation))
+        self.assertTrue("u_s_precipitation" in self.ds.unc.var_unc_var_names("precipitation"))
+
+        np.testing.assert_array_equal(u_s_precipitation.values, self.ds.u_s_precipitation.values)
 
 
 if __name__ == "__main__":
