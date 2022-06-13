ecb7b1492ff39aa3083c10dc8f85a4dae062f19f
shunt16 <sam.hunt@npl.co.uk>
2022-06-13 17:04:21 +0100

    define err_corr_forms registration decorator and initial err corr foms

--- obsarray/err_corr_forms.py
+++ obsarray/err_corr_forms.py
@@ -2,9 +2,10 @@
 
 import abc
 from typing import Callable, Type
+import numpy as np
 
 __author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
-__all__ = []
+__all__ = ["err_corr_forms"]
 
 
 class ErrCorrForms:
@@ -42,12 +43,72 @@ class BaseErrCorrForm(abc.ABC):
     Base class for error-correlation parameterisation form defintions
     """
 
-    def __init__(self):
+    def __init__(self, xarray_obj, unc_var_name, params, units):
+        self._obj = xarray_obj
+        self.unc_var_name = unc_var_name
+        self.params = params if isinstance(params, list) else [params]
+        self.units = units
+
+    def __str__(self):
+        """Custom __str__"""
+
+        return self.form + " " + str(self.params)
+
+    def __repr__(self):
+        """Custom  __repr__"""
+        return str(self)
+
+    @property
+    @abc.abstractmethod
+    def form(self) -> str:
+        """Form name"""
         pass
 
     @abc.abstractmethod
-    def form(self):
+    def build_matrix(self, idx: np.ndarray) -> np.ndarray:
         pass
+
+
+def register_err_corr_form(form_name: str) -> Callable:
+    """
+    Decorator for registering error-correlation parmaterisation form definition to package definition container
+
+    :param form_name: name of error-correlation parmaterisation form e.g. "random"
+    :return: decorator function
+    """
+
+    def decorator(form_cls: Type) -> Type:
+        err_corr_forms[form_name] = form_cls
+        return form_cls
+
+    return decorator
+
+
+@register_err_corr_form("random")
+class RandomForm(BaseErrCorrForm):
+
+    form = "random"
+
+    def build_matrix(self, x):
+        return np.eye(len(x))
+
+
+@register_err_corr_form("systematic")
+class SystematicForm(BaseErrCorrForm):
+
+    form = "systematic"
+
+    def build_matrix(self, idx):
+        return np.ones((len(idx), len(idx)))
+
+
+@register_err_corr_form("custom")
+class CustomForm(BaseErrCorrForm):
+
+    form = "custom"
+
+    def build_matrix(self, idx):
+        return self._obj[self.params[0]]
 
 
 if __name__ == "__main__":
