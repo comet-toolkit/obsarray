b31fa09d3d8893bdd739c38aa4946f7c66df7bbf
sam.hunt <sam.hunt@npl.co.uk>
2024-11-07 15:05:30 +0000

    initial commit of obs_concat defining interface

---
+++ obsarray/concat.py
@@ -0,0 +1,53 @@
+"""obsarray.concat - module with extension to xarray.concat for obs_vars and unc_vars"""
+
+import numpy as np
+import xarray as xr
+from typing import Union, Any
+from xarray.core.types import T_Dataset, T_DataArray, T_Variable
+from collections.abc import Hashable, Iterable
+
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+__all__ = []
+
+
+def obs_concat(
+    objs: Iterable[T_DataArray],
+    dim: Union[Hashable, T_Variable, T_DataArray, Any],
+    unc: T_Dataset,
+    dim_err_corr: Iterable[Any],
+    combine_unc: str = "concat",
+    *args,
+    **kwargs
+):
+    """
+    Concatenate xarray *obs_vars* along a new or existing dimension, safely handling also
+    concatenating associated *unc_vars*. Extension to :py:func:`xarray.concat`.
+
+    :param objs: sequence of :py:class:`xarray.Dataset` and :py:class:`xarray.DataArray`
+        xarray objects to concatenate *obs_vars* together. As for :py:class:`xarray.Dataset`,
+        each object is expected to consist of variables and coordinates with matching shapes
+        except for along the concatenated dimension.
+    :param dim: Name of the dimension to concatenate along. This can either be a new
+        dimension name, in which case it is added along axis=0, or an existing
+        dimension name, in which case the location of the dimension is
+        unchanged. If dimension is provided as a Variable, DataArray or Index, its name
+        is used as the dimension to concatenate along and the values are added
+        as a coordinate.
+    :param unc: dataset containing the unc_vars associated with objs
+    :param dim_err_corr: error-correlation form definition for concatenation dimension
+    :param combine_unc: string indicating how to concatenate unc_vars.
+
+        * "concat": (default) merges *unc_vars* as for *obs_vars* - assumes *unc_var* order is the same between *obs_vars*
+        * "no_concat": expands each *unc_var* along dim, gap filling with zeros
+    """
+
+    concat_obs_vars = xr.concat(objs, dim, *args, **kwargs)
+    concat_unc_vars = None
+    return concat_obs_vars, concat_unc_vars
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ obsarray/test/test_concat.py
@@ -0,0 +1,202 @@
+"""test_err_corr_forms - tests for obsarray.err_corr_forms"""
+
+import unittest
+import numpy as np
+import obsarray
+import xarray as xr
+
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+__all__ = []
+
+from obsarray.concat import obs_concat
+
+
+def create_test_ds():
+    c1a = np.ones((4, 3))
+    c2a = np.ones((4, 3))
+
+    c1b = np.ones((7, 5, 3))
+    c2b = np.ones((7, 5, 3))
+
+    d1a = np.ones((4, 3)) * 1
+    d2a = np.ones((4, 3)) * 2
+    s1a = np.ones((4, 3)) * 3
+    s2a = np.ones((4, 3)) * 4
+    s3a = np.ones((4, 3)) * 5
+
+    d1b = np.ones((7, 5, 3)) * 1
+
+    d1a_attrs = {"units": "test_units", "geometry": "a", "measurand": "d", 'm': 10}
+    d2a_attrs = {"units": "test_units", "geometry": "a", "measurand": "d", 'm': 11}
+    s1a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 12}
+    s2a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 4}
+    s3a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 5}
+    d1b_attrs = {"units": "test_units", "geometry": "b", "measurand": "d"}
+
+    ds = xr.Dataset(
+        {
+            "d1a": (["xa", "ya"], d1a, d1a_attrs),
+            "d2a": (["xa", "ya"], d2a, d2a_attrs),
+            "s1a": (["xa", "ya"], s1a, s1a_attrs),
+            "s2a": (["xa", "ya"], s2a, s2a_attrs),
+            "s3a": (["xa", "ya"], s3a, s3a_attrs),
+            "d1b": (["xb", "yb", "zb"], d1b, d1b_attrs),
+        },
+        coords={
+            "c1a": (["xa", "ya"], c1a),
+            "c2a": (["xa", "ya"], c2a),
+            "c1b": (["xb", "yb", "zb"], c1b),
+            "c2b": (["xb", "yb", "zb"], c2b),
+        },
+        attrs={
+            "history": "test_history",
+            "meas_vars": ["d1a", "d2a", "s1a", "s2a", "s3a", "d1b"],
+        },
+    )
+
+    for var in ["d1a", "d2a"]:
+        ds.unc[var]["u_r_" + var] = (["xa", "ya"], ds[var].values, {})
+
+        err_corr_def = [
+            {
+                "dim": ["xa", "ya"],
+                "form": "systematic",
+                "params": [],
+                "units": []
+            }
+        ]
+
+        ds.unc[var]["u_s_" + var] = (["xa", "ya"], ds[var].values, {"err_corr": err_corr_def})
+
+    return ds
+
+def create_concat_ds():
+    c1a = np.ones((4, 3))
+    c2a = np.ones((4, 3))
+
+    c1b = np.ones((7, 5, 3))
+    c2b = np.ones((7, 5, 3))
+
+    da = np.ones((4, 3, 2)) * 1
+    da[:, : 1] = 2
+
+    s1a = np.ones((4, 3)) * 3
+    s2a = np.ones((4, 3)) * 4
+    s3a = np.ones((4, 3)) * 5
+
+    d1b = np.ones((7, 5, 3)) * 1
+
+    da_attrs = {"units": "test_units", "geometry": "a", "measurand": "d", 'm': 10}
+    s1a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 12}
+    s2a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 4}
+    s3a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 5}
+    d1b_attrs = {"units": "test_units", "geometry": "b", "measurand": "d"}
+
+    ds = xr.Dataset(
+        {
+            "da": (["xa", "ya"], da, da_attrs),
+            "s1a": (["xa", "ya"], s1a, s1a_attrs),
+            "s2a": (["xa", "ya"], s2a, s2a_attrs),
+            "s3a": (["xa", "ya"], s3a, s3a_attrs),
+            "d1b": (["xb", "yb", "zb"], d1b, d1b_attrs),
+        },
+        coords={
+            "c1a": (["xa", "ya"], c1a),
+            "c2a": (["xa", "ya"], c2a),
+            "c1b": (["xb", "yb", "zb"], c1b),
+            "c2b": (["xb", "yb", "zb"], c2b),
+        },
+        attrs={
+            "history": "test_history",
+            "meas_vars": ["d1a", "d2a", "s1a", "s2a", "s3a", "d1b"],
+        },
+    )
+
+    for var in ["d1a", "d2a"]:
+        ds.unc[var]["u_r_" + var] = (["xa", "ya"], ds[var].values, {})
+
+        err_corr_def = [
+            {
+                "dim": ["xa", "ya"],
+                "form": "systematic",
+                "params": [],
+                "units": []
+            }
+        ]
+
+        ds.unc[var]["u_s_" + var] = (["xa", "ya"], ds[var].values, {"err_corr": err_corr_def})
+
+    return ds
+
+
+
+
+class TestConcat(unittest.TestCase):
+
+    def test_concat_combine_unc_concat(self):
+        ds = create_test_ds()
+
+        obs_vars, unc_vars = obs_concat([ds["d1a"], ds["d2a"]], "new_dim", ds, "concat")
+
+        c1a = np.ones((4, 3))
+        c2a = np.ones((4, 3))
+
+        c1b = np.ones((7, 5, 3))
+        c2b = np.ones((7, 5, 3))
+
+        da = np.ones((4, 3, 2)) * 1
+        da[:, : 1] = 2
+
+        s1a = np.ones((4, 3)) * 3
+        s2a = np.ones((4, 3)) * 4
+        s3a = np.ones((4, 3)) * 5
+
+        d1b = np.ones((7, 5, 3)) * 1
+
+        da_attrs = {"units": "test_units", "geometry": "a", "measurand": "d", 'm': 10}
+        s1a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 12}
+        s2a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 4}
+        s3a_attrs = {"units": "test_units", "geometry": "a", "measurand": "s", 'm': 5}
+        d1b_attrs = {"units": "test_units", "geometry": "b", "measurand": "d"}
+
+        ds = xr.Dataset(
+            {
+                "da": (["xa", "ya"], da, da_attrs),
+                "s1a": (["xa", "ya"], s1a, s1a_attrs),
+                "s2a": (["xa", "ya"], s2a, s2a_attrs),
+                "s3a": (["xa", "ya"], s3a, s3a_attrs),
+                "d1b": (["xb", "yb", "zb"], d1b, d1b_attrs),
+            },
+            coords={
+                "c1a": (["xa", "ya"], c1a),
+                "c2a": (["xa", "ya"], c2a),
+                "c1b": (["xb", "yb", "zb"], c1b),
+                "c2b": (["xb", "yb", "zb"], c2b),
+            },
+            attrs={
+                "history": "test_history",
+                "meas_vars": ["d1a", "d2a", "s1a", "s2a", "s3a", "d1b"],
+            },
+        )
+
+        for var in ["d1a", "d2a"]:
+            ds.unc[var]["u_r_" + var] = (["xa", "ya"], ds[var].values, {})
+
+            err_corr_def = [
+                {
+                    "dim": ["xa", "ya"],
+                    "form": "systematic",
+                    "params": [],
+                    "units": []
+                }
+            ]
+
+            ds.unc[var]["u_s_" + var] = (["xa", "ya"], ds[var].values, {"err_corr": err_corr_def})
+
+        return ds
+
+
+
+if __name__ == "__main__":
+    pass
